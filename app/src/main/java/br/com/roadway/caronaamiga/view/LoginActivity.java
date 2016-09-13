7086b9d5a449cc9b50f1e202606bad6ca8d44294
César Rodrigues <rasec1287@hotmail.com>
2016-09-13 11:20:34 -0300

    Adicionada configuração incial do login com o Facebook

---
+++ app/src/main/java/br/com/roadway/caronaamiga/view/LoginActivity.java
@@ -0,0 +1,19 @@
+package br.com.roadway.caronaamiga.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+import br.com.roadway.caronaamiga.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(getApplication());
+        setContentView(R.layout.activity_login);
+    }
+}
